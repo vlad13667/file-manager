2e9a56f3832812f3c8a00d1a12c5c67aad3ad465
Perceval <93855736+vlad13667@users.noreply.github.com>
2023-11-12 09:44:02 +0500

    Вторая часть
    Сделано
    -Разработать метод скачивания файла по прямой ссылке
    -Разработать метод, возвращающий список моделей данных (кроме самого файла, но с полем со ссылкой на скачивание файла)
    -К предыдущему методу добавить возможность фильтрации
    •	по имени (возможно частичное вхождение)
    •	по дате изменения (диапазон от и до)(не протестировано)
    •	по типу (без указания - все, при указании - список переданных типов)
    -Добавить валидацию загружаемого файла по типу и размеру. Тип - распространенные форматы документов, размер - 15 Мб(не протестировано)
    -Разработать метод скачивания нескольких указанных файлов в одном архиве
    В разработке
    е.	Сделать обработку различных Runtime Exceptions (ошибок обработки, валидации и т.д.)

--- src/main/java/com/example/controller/FilesController.java
+++ src/main/java/com/example/controller/FilesController.java
@@ -1,12 +1,22 @@
 package com.example.controller;
 
+
 import java.io.IOException;//исключения
+import java.io.InputStream;
+import java.time.LocalDateTime;
+
 import java.util.List;//коллекция List
+
 
 
 import com.example.model.FileData;
 
+
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.format.annotation.DateTimeFormat;
+
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;//для работы с загружаемыми файлами в Spring
@@ -17,20 +27,41 @@ import com.example.service.FileService;
 
 public class FilesController  {
 
+
+
+    private static final long MAX_FILE_SIZE = 15 * 1024 * 1024; // 15 Мб
+    private static java.util.Arrays Arrays;
+    private static final List<String> ALLOWED_FILE_TYPES = Arrays.asList(
+                // Список разрешенных MIME-типов файлов
+                "application/msword", // .doc
+                "application/vnd.openxmlformats-officedocument.wordprocessingml.document", // .docx
+                "application/vnd.ms-excel", // .xls
+                "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", // .xlsx
+                "application/pdf", // .pdf
+                "text/plain", // .txt
+                "application/vnd.ms-powerpoint", // .ppt
+                "application/vnd.openxmlformats-officedocument.presentationml.presentation" // .pptx
+        );
     @PostMapping("/files")
     public ResponseEntity<String> uploadFile(@RequestParam("file") MultipartFile uploadedFile) throws IOException {
+        if (uploadedFile.getSize() > MAX_FILE_SIZE) {
+            return new ResponseEntity<>("Ошибка: размер файла превышает 15 Мб", HttpStatus.BAD_REQUEST);
+        }
+
+        if (!ALLOWED_FILE_TYPES.contains(uploadedFile.getContentType())) {
+            return new ResponseEntity<>("Ошибка: недопустимый тип файла", HttpStatus.BAD_REQUEST);
+        }
         FileData file = FileService.save(uploadedFile);
 
         return new ResponseEntity<>("Успешная загрузка файла " + file.getFileName(), HttpStatus.CREATED);
 
     }
 
-    @GetMapping ("/files")
-    public ResponseEntity<List<String>> getFileNames() throws IOException {
-        List<String> file =  FileService.loadAll();
-        return ResponseEntity.ok(file);
+    @GetMapping("/files/download/multiple")
+    public ResponseEntity<byte[]> downloadMultipleFiles(@RequestParam List<String> fileNames) {
+       FileService.multupla(fileNames);
+        return null;
     }
-
     @GetMapping ("/files/{fileName}")
 
     public ResponseEntity<?> getFileByName(@PathVariable String fileName) throws IOException {
@@ -40,6 +71,33 @@ public class FilesController  {
         return file != null ?
                 new ResponseEntity(file, HttpStatus.OK)
                 : new ResponseEntity<>("Файл с именем '" + fileName + "' не найден", HttpStatus.NOT_FOUND);
+    }
+    @GetMapping("/files/download/{fileName}")
+    public ResponseEntity<InputStreamResource> downloadFile(@PathVariable String fileName) throws IOException {
+        try {
+            // Эта строка получает  файл как InputStream от  сервиса.
+
+            InputStream file = FileService.download(fileName);
+
+            //Устанавливаем contentType файла как MediaType.APPLICATION_OCTET_STREAM, чтобы файл мог быть прямо загружен
+            return ResponseEntity.ok()
+                    .contentType(MediaType.APPLICATION_OCTET_STREAM)
+                    .header("Content-Disposition", "attachment;filename=" + fileName)
+                    .body(new InputStreamResource(file));
+        } catch (IOException e) {
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+    @RequestMapping(value = "/files", method = RequestMethod.GET)
+    public ResponseEntity<List<FileData>> getFileNames(
+            @RequestParam(required = false) String name,
+            @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime dateFrom,
+            @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime dateTo,
+            @RequestParam(required = false) List<String> types) throws IOException {
+
+        List<FileData> files = FileService.loadAllFiltered(name, dateFrom, dateTo, types);
+        return ResponseEntity.ok(files);
     }
     /*
     @PutMapping("/files/{fileName}")
--- src/main/java/com/example/service/FileService.java
+++ src/main/java/com/example/service/FileService.java
@@ -1,12 +1,20 @@
 package com.example.service;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-
+import java.io.InputStream;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 import com.example.model.FileData;
-import org.springframework.beans.factory.annotation.Value;
+import org.apache.commons.io.IOUtils;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -15,10 +23,46 @@ public class FileService{
     private static List<FileData> files = new ArrayList<>();
 
 
-    @Value("${upload.path}") //вставляет путь из application.properties
-    private String uploadPath;
 
 
+
+public static ResponseEntity<byte[]> multupla(List<String> fileNames)
+{
+    try {
+        // поток который записывает содержимое файла в байты
+        ByteArrayOutputStream byteOutputStream = new ByteArrayOutputStream();
+        // записывает данные в zip
+        ZipOutputStream zipOutputStream = new ZipOutputStream(byteOutputStream);
+
+        for (String fileName : fileNames) {
+
+            InputStream fileStream = FileService.download(fileName);
+
+            // Создает новый ZipEntry с указанным именем файла и добавляет его в zip-архив.
+            ZipEntry zipEntry = new ZipEntry(fileName);
+            zipOutputStream.putNextEntry(zipEntry);
+
+            // копирования содержимого файла (fileStream) в сжатый zip-поток (zipOutputStream).
+            IOUtils.copy(fileStream, zipOutputStream);
+            fileStream.close();
+            zipOutputStream.closeEntry();
+        }
+
+        zipOutputStream.close();
+        byte[] bytes = byteOutputStream.toByteArray();
+        //Создается новый экземпляр HttpHeaders и добавляет в него заголовок Content-Disposition. Это делает файл доступным для загрузки с именем "files.zip".
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=files.zip");
+
+        return ResponseEntity.ok()
+                .headers(headers)
+                .body(bytes);
+
+    } catch (IOException e) {
+        e.printStackTrace();
+        return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+    }
 
 
     public static FileData save(MultipartFile uploadedFile) throws IOException {
@@ -50,7 +94,7 @@ public class FileService{
                 return file;
             }
         }
-        throw new IllegalArgumentException("File "+ fileName + " not found");
+        throw new IllegalArgumentException("Файл "+ fileName + " не найден");
     }
 
         // удаление всех файлов
@@ -60,6 +104,7 @@ public class FileService{
 
     }
 
+    //удаление файла по имени
     public static boolean delete(String fileName) throws IOException {
         FileData foundFile = null;
         for (FileData file : files) {
@@ -77,18 +122,58 @@ public class FileService{
        return false;
     }
 
-
-
-    public static List<String> loadAll() throws IOException {
-        List<String> fileNames = new ArrayList<>();
+    //Скачивание файла
+    public static InputStream download(String fileName) throws IOException {
+        FileData foundFile = null;
         for (FileData file : files) {
-            fileNames.add(file.getFileName());
+            if (file.getFileName().equals(fileName)) {
+                foundFile = file;
+                break;
+            }
         }
-        return(fileNames);
-
+        if (foundFile != null) {
+            byte[] fileContent = foundFile.getFileContent();
+            return new ByteArrayInputStream(fileContent);
+        }
+        throw new IllegalArgumentException("Файл " + fileName + " не найден");
     }
 
 
+    public static List<FileData> loadAllFiltered(String name, LocalDateTime dateFrom, LocalDateTime dateTo, List<String> types) {
+        List<FileData> fileDataList = new ArrayList<>();
+
+        for (FileData file : files) {
+            // проверка на подстроку
+            boolean matchesName = name == null || file.getFileName().contains(name);
+
+            LocalDateTime uploadDate = file.getUploadDate().toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime();
+
+            boolean matchesDate =
+                    (dateFrom == null || uploadDate.isAfter(dateFrom) || uploadDate.isEqual(dateFrom)) &&
+                            (dateTo == null || !uploadDate.isAfter(dateTo));
+
+            boolean matchesType = types == null || types.contains(file.getFileType());
+
+            if (matchesName && matchesDate && matchesType) {
+                FileData fileDataCopy = new FileData();
+                fileDataCopy.setUploadDate(file.getUploadDate());
+                fileDataCopy.setChangeDate(file.getChangeDate());
+                fileDataCopy.setFileName(file.getFileName());
+                fileDataCopy.setFileType(file.getFileType());
+                fileDataCopy.setFileSize(file.getFileSize());
+                fileDataCopy.setFileUrl("/files/download/" + file.getFileName());
+                fileDataCopy.setFileContent(file.getFileContent());
+
+                fileDataList.add(fileDataCopy);
+            }
+        }
+
+        if (fileDataList.isEmpty()){
+            throw new IllegalArgumentException("Ни один файл не соответствует предоставленным фильтрам");
+        }
+
+        return fileDataList;
+    }
 
 
 }
